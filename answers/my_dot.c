2ff8face76df2accb1fc72ac1d6c467ca21fbe46
Alexey Svyatkovskiy <asvyatkovskiy@gmail.com>
2016-10-18 23:22:26 -0400

    Keep only installation instructions in the master

--- answers/my_dot.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-double dot_product(double v[], double u[], int n)
-{
-    double result = 0.0;
-    for (int i = 0; i < n; i++)
-        result += v[i]*u[i];
-    return result;
-}
