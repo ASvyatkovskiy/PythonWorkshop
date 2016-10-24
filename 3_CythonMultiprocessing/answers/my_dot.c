1de051c0948b857c4cef5aae6824568ae31d062c
Alexey Svyatkovskiy <asvyatkovskiy@gmail.com>
2016-10-24 14:00:11 -0400

    Cython, interfacing with compiled code, multiproc

---
+++ 3_CythonMultiprocessing/answers/my_dot.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+double dot_product(double v[], double u[], int n)
+{
+    double result = 0.0;
+    for (int i = 0; i < n; i++)
+        result += v[i]*u[i];
+    return result;
+}
